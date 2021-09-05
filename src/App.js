187ac54a9e614948b44adbcd50675a00a25d5eb9
Cleferson Dannylo <clefersonnascimento8@gmail.com>
2021-09-04 23:43:29 -0300

    uso do useCallback e do useMemo

--- src/App.js
+++ src/App.js
@@ -2,16 +2,8 @@ import { useState } from 'react'
 import List from './components/List'
 // import Button from './components/Button'
 
-export default function App() {
+export default function App(props) {
   const [showList, setShowList] = useState(true)
 
-  return (
-    <>
-      {showList && <List />}
-      {/* <button onClick={() => setShowList(preventShowList => !preventShowList)}>
-        {' '}
-        Clique
-      </button> */}
-    </>
-  )
+  return <>{showList && <List />}</>
 }
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,19 +1,23 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useCallback, useMemo } from 'react'
 
-export default function List() {
+const set = new Set()
+
+export default function List(props) {
   const [counter, setCounter] = useState(0)
+  const number = useMemo(() => {
+    return 4245454564454545454454 * 45646545455645596897454564
+  }, []) //Use o useMemo para cálculos pesados ou para repassar a variável através das props....Retorna uma variável.
+  const handleSetCounter = useCallback(() => {
+    setCounter(preventCounter => (preventCounter += 1)) //passo uma função anônima para pegar o valor atual e aumentar desse valor.
+    set.add(handleSetCounter)
+  }, []) //Posso usá-lo para evitar renderizações desnecessárias, fazendo com que realmente a mesma função seja executada a cada clique.
+  //Usada para passar nossa função para outros componentes (via props)....retorna função.
 
+  console.log(set.size)
   useEffect(() => {
-    return () => {
-      console.log('atualizou')
-    } //Apesar de usar o return para o trecho de código ser executado apenas na hora que o componente for desmontado, por conta da atribuição de valor da variável que está no array, ele será executado bem antes do horário "previsto".
+    return () => {} //Apesar de usar o return para o trecho de código ser executado apenas na hora que o componente for desmontado, por conta da atribuição de valor da variável que está no array, ele será executado bem antes do horário "previsto".
   }, [counter])
 
-  // useEffect(() => {
-  //   return () => {
-  //     console.log('Desmontado')
-  //   }
-  // }, [])
   return (
     <div
       style={{
@@ -23,10 +27,8 @@ export default function List() {
       }}
     >
       {counter}
-      <button
-        style={{ margin: '1rem' }}
-        onClick={() => setCounter(preventCounter => preventCounter + 1)} //passo uma função anônima para pegar o valor atual e aumentar desse valor.
-      >
+
+      <button style={{ margin: '1rem' }} onClick={handleSetCounter}>
         Clique
       </button>
     </div>
