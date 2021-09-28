f63a3998528bf1c13ac9678385c346c1c9adba12
Cleferson Dannylo <clefersonnascimento8@gmail.com>
2021-09-27 22:20:11 -0300

    usando o sessionStorage e o localStorage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
-import { useState } from 'react'
-import List from './components/List'
+import { useState, useEffect } from 'react'
+import api from '../src/services/api'
 
 export default function App() {
-  const [showList, setShowList] = useState(true)
+  const [username, setUsername] = useState('')
+  const [userData, setUserData] = useState({})
   useEffect(() => {
-    async function setUserResponse() {
-      await localStorage.setItem('user', JSON.stringify({}))
-    }
-    setUserResponse()
+    const localStorageUserData = localStorage.getItem(
+      '@reactapp:githubUserData'
+    )
+    setUserData(JSON.parse(localStorageUserData) || {})
   }, [])
 
-  return <>{showList && <List />}</>
+  async function getUserGithubData() {
+    const { data } = await api.get(username)
+    localStorage.setItem('@reactapp:githubUserData', JSON.stringify(data))
+    setUserData(data)
+  }
+
+  // const [showList, setShowList] = useState(true)
+  // useEffect(() => {
+  //   async function setUserResponse() {
+  //     await localStorage.setItem('user', JSON.stringify({}))
+  //   }
+  //   setUserResponse()
+  // }, [])
+
+  return (
+    <>
+      <input
+        type="text"
+        onChange={event => setUsername(event.target.value)}
+        value={username}
+      />
+      <button onClick={getUserGithubData}>Pesquisar usuário</button>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column'
+        }}
+      >
+        <div>{userData.name}</div>
+        <div>{userData.bio}</div>
+      </div>
+    </>
+  )
 }
